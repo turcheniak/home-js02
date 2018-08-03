9432c2f82f6e0d5b76177342f6dfe71654bba607
turcheniak <turcheniak.oleg@gmail.com>
2018-08-03 11:01:10 +0000

    fibonachi

---
+++ js/index.js
@@ -0,0 +1,14 @@
+const number = prompt('Введіть число фібоначчі :');
+
+function fibonachi (n) {
+  let a = 1;
+  let b = 1;
+  for (let i = 3; i <= n; i++) {
+    let c = a + b;
+    a = b;
+    b = c;
+  }
+  return b;
+}
+
+alert('Число ' + fibonachi() + ' Фібоначчі');
